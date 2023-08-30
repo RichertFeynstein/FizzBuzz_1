6111d93d4641774e2a5c62403b389106357ee03a
RichertFeynstein <Richert.feynstein.edu1@gmail.com>
2023-08-30 09:42:34 +0530

    Made more changes

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -4,16 +4,28 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Enter an integer: ', function(input) {
-  const integer = parseInt(input);
-  if (integer % 3 === 0 && integer % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (integer % 3 === 0) {
-    console.log("Fizz");
-  } else if (integer % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(integer);
-  }
-  rl.close();
-});
+function fizzBuzzLoop() {
+  rl.question('Enter an integer (or type "exit" to quit): ', function(input) {
+    if(input.toLowerCase() === 'exit') {
+      rl.close();
+      return;
+    }
+  
+    const integer = parseInt(input);
+    
+    if (integer % 3 === 0 && integer % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (integer % 3 === 0) {
+      console.log("Fizz");
+    } else if (integer % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(integer);
+    }
+
+    // Call the function again to get another input
+    fizzBuzzLoop();
+  });
+}
+
+fizzBuzzLoop();
